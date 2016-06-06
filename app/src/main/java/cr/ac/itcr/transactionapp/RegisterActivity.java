1d4a371f0e068a8469d4679d99977cd7ba618719
Yorbi Mendez <yorbigmendez@gmail.com>
2016-06-06 16:27:01 -0600

    Register Activity changes

--- app/src/main/java/cr/ac/itcr/transactionapp/RegisterActivity.java
+++ app/src/main/java/cr/ac/itcr/transactionapp/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 
 import cr.ac.itcr.transactionapp.entity.User;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity{
     private EditText txtUsername;
     private EditText txtPassword;
     private EditText txtEmail;
@@ -36,7 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(txtUsername.getText().toString().equals("") || txtPassword.getText().toString().equals("") || txtEmail.getText().toString().equals("") || txtDebit.getText().toString().equals("")){
+                if(canRegister() && txtUsername.getText().toString().equals("") || txtPassword.getText().toString().equals("") || txtEmail.getText().toString().equals("") || txtDebit.getText().toString().equals("")){
                     Log.e("unable to register...","Im sorry... try again");
                     Intent intent = new Intent(getApplicationContext(), Dashboard.class);
                     startActivity(intent);
@@ -47,6 +49,26 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public boolean canRegister(){
+        ArrayList<User> users = new ArrayList<>();
+
+        for(int i=0; i<users.size(); i++){
+            if(users.get(i).getUser().equals(txtUsername.getText().toString())){
+                setErrorMessage(txtUsername,"Username unavailable");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void emptyError(EditText txt){
+        txt.setError(null);
+    }
+
+    public void setErrorMessage(EditText txt, String errorMsg){
+        txt.setError(errorMsg);
     }
 
 
